b55f8ec881f5e9430732523f5fd8003d9e3cfab7
Lucianofalco <lucianosebastianfalco@gmail.com>
2023-03-21 13:26:06 -1200

    new HolaMundo

---
+++ tp1_1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int a ; 
+    printf("Hola mundo\n");
+    scanf ("%d" , &a);
+    printf("\n %d" , a);
+    return 0;
+}
+ 
+
